bdefaf5266bef86f91875091c89a451951e099c3
auro <auro@DESKTOP-AV8I9BT>
2024-08-25 20:55:46 +0300

    Add project files.

---
+++ ImdbListPractice/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImdbListPractice
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public double ImdbScore { get; set; }
+
+        public Movie(string movieName, double imdbScore)
+        {
+            Name = movieName;
+            ImdbScore = imdbScore;
+        }
+
+
+    }
+}
---
+++ ImdbListPractice/Program.cs
@@ -0,0 +1,48 @@
+﻿using ImdbListPractice;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Movie> movieList = new List<Movie>();
+        while (true)
+        {
+            Console.Write("Lütfen film adı giriniz. Çıkmak için 'h' tuşuna basınız.");
+            string choiseName = Console.ReadLine().ToLower();
+            if (choiseName == "h")
+                break;
+            Console.Write("Filmin IMDB puanını giriniz.");
+            double choiseImdb;
+            while (!double.TryParse(Console.ReadLine(), out choiseImdb))
+            {
+                Console.Write("Lütfen geçerli bir IMDB puanı girin: ");
+            }
+            Movie movie = new Movie(choiseName , choiseImdb );
+            movieList.Add(movie);
+        }
+        Console.WriteLine("******Seçilen Filmler******");
+        foreach (var Movie in movieList)
+        {
+            Console.WriteLine($"Seçilen Film -> {Movie.Name}" +
+                              $"\nImdb Puanı -> {Movie.ImdbScore}");
+        }
+        Console.WriteLine("\n*** IMDB Puanı 4 ile 9 arasında olan filmler ***");
+        foreach (var Movie in movieList)
+        {
+            if (Movie.ImdbScore >= 4 && Movie.ImdbScore <= 9)
+                Console.WriteLine($"Film Adı -> {Movie.Name}, IMDB Puanı -> {Movie.ImdbScore}");
+        }
+        Console.WriteLine("\n*** 'A' harfi ile başl filmler ***");
+        foreach (var Movie in movieList)
+        {
+            if(Movie.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine($"Filmin Adı -> {Movie.Name}" +
+                                  $"\n Imdb Puanı -> {Movie.ImdbScore}");
+        }
+
+
+
+    }
+
+
+}
